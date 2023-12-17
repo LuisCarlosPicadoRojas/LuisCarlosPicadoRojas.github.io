9849a3d89eeded2c67f636d21b89f4fc1811e62f
LuisCarlosPicadoRojas <144180800+LuisCarlosPicadoRojas@users.noreply.github.com>
2023-12-17 00:49:02 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-function mostrarProyectos(tecnologia) {
-    const proyectos = document.querySelectorAll('.project');
-  
-    proyectos.forEach((proyecto) => {
-      const tieneTecnologia = proyecto.classList.contains(tecnologia);
-      const esMostrarTodos = tecnologia === 'all';
-  
-      if (esMostrarTodos || tieneTecnologia) {
-        proyecto.style.display = 'block';
-      } else {
-        proyecto.style.display = 'none';
-      }
+document.addEventListener('DOMContentLoaded', function() {
+  const enlaces = document.querySelectorAll('.myself__footer__link');
+
+  enlaces.forEach(enlace => {
+    enlace.addEventListener('click', function(event) {
+      event.preventDefault();
+      this.classList.toggle('clicked');
     });
-  }
-  
-  function mostrarTodos() {
-    mostrarProyectos('all');
-  }
+  });
+});
+
+function mostrarProyectos(tecnologia) {
+  const proyectos = document.querySelectorAll('.project');
+
+  proyectos.forEach((proyecto) => {
+    const tieneTecnologia = proyecto.classList.contains(tecnologia);
+    const esMostrarTodos = tecnologia === 'all';
+
+    if (esMostrarTodos || tieneTecnologia) {
+      proyecto.style.display = 'block';
+    } else {
+      proyecto.style.display = 'none';
+    }
+  });
+}
+
+function mostrarTodos() {
+  mostrarProyectos('all');
+}
